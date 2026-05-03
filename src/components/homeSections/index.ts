9d1077ec35e0d29781bdcb3ebfa3cdff9f318ee4
sm-mazharul-islam <dev.sm.mazharul@gmail.com>
2026-05-03 16:15:39 +0600

    Add: index ts

---
+++ src/components/homeSections/index.ts
@@ -0,0 +1,18 @@
+// Shared Components
+export { default as Header } from "../shared/Header";
+export { default as Footer } from "../shared/Footer";
+export { default as Container } from "../shared/Container";
+
+// Feature/UI Components
+export { default as Features } from "../features/Features";
+export { default as HomeFlashSale } from "../homeFlashSale/HomeFlashSale";
+export { default as TopCategoriesCard } from "../ui/TopCategoriesCard";
+export { default as HomeProducts } from "../homeProducts/HomeProducts";
+export { default as Testimonials } from "../testimonials/Testimonials";
+
+// New Sections
+export { default as Partners } from "../home/partners/Partners";
+export { default as BlogPreview } from "../home/blogPreview/BlogPreview";
+export { default as Newsletter } from "../home/newsletter/NewsLetter";
+export { default as CTA } from "../home/callToAction/Cta";
+export { default as Faq } from "../home/faq/Faq";
