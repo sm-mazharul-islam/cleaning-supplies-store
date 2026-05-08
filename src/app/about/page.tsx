2287e23cef5802571faa0ee1bb5c782b88f9029f
sm-mazharul-islam <dev.sm.mazharul@gmail.com>
2026-05-08 16:47:29 +0600

    Update: About page theme

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -17,9 +17,9 @@ const AboutPage = () => {
   return (
     <>
       <NavbarTwo />
-      <div className="bg-white overflow-hidden">
+      <div className=" overflow-hidden">
         {/* 1. HERO SECTION */}
-        <section className="relative py-20 lg:py-32 bg-[#F8FAFC]">
+        <section className="relative py-20 lg:py-32 ">
           <div className="max-w-7xl mx-auto px-6 grid grid-cols-1 lg:grid-cols-2 gap-16 items-center">
             <div className="animate-in slide-in-from-left duration-1000">
               <span className="bg-blue-100 text-blue-600 px-4 py-2 rounded-full text-xs font-black uppercase tracking-widest">
